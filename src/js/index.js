121b18f98f6ad043055474303450509dda738d55
da30 <da30@illinois.edu>
2025-09-05 00:09:20 -0500

    Final Submssion

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
-/* Simple theme toggle functionality */
 const checkbox = document.getElementById('theme-toggle');
 const toggleIcon = document.querySelector('.toggle-icon');
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
-  
-  // Update icon based on checkbox state
   if (checkbox.checked) {
     toggleIcon.textContent = '☀️';
   } else {
     toggleIcon.textContent = '🌙';
   }
 });
+
+// This inspiration for this was taken from the following website: https://codepen.io/Umer_Farooq/pen/eYJgKGN 
+// and was implemented with the help of ChatGPT. The link to the ChatGPT conversation is:
+// https://chatgpt.com/share/68ba6bd9-b248-800b-90a2-48c36facc9a5
